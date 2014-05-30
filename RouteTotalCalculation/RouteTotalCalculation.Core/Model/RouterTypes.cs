0835a0d307bc0db99720acf976a2424d99962e05
Rubens Koji Miura <rubenskm@yahoo.com.br>
2014-05-30 14:59:40 -0300

    Removing file

--- RouteTotalCalculation/RouteTotalCalculation.Core/Model/RouterTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RouteTotalCalculation.Core.Model
-{
-	public enum RouterTypes
-	{
-		DefaultQuickestRoute = 0, // Rota padrão mais rápida
-		RouteAvoidingTraffic = 23 // Rota evitando o trânsito
-	}
-}
